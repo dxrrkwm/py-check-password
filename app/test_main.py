a3ef326a2e640a5f6635ccdc2ebeae3e2ab1b111
Vadym Sulim <ohnooq1@gmail.com>
2025-01-30 22:27:45 +0200

    fixed linter errors

--- app/test_main.py
+++ app/test_main.py
@@ -4,17 +4,17 @@ from app.main import check_password
 
 
 @pytest.mark.parametrize("password, expected", [
-    ('Pass@word1', True),
-    ('Password@', False),
-    ('Password1', False),
-    ('password@1', False),
-    ('Pass@1', False),
-    ('Pass@word1Pass@word1', False),
-    ('Pass@word1!', False),
-    ('A1@bcdef', True),
-    ('A1@bcdefghijklmn', True),
-    ('A1@bcde', False),
-    ('A1@bcdefghijklmno', False),
+    ("Pass@word1", True),
+    ("Password@", False),
+    ("Password1", False),
+    ("password@1", False),
+    ("Pass@1", False),
+    ("Pass@word1Pass@word1", False),
+    ("Pass@word1!", False),
+    ("A1@bcdef", True),
+    ("A1@bcdefghijklmn", True),
+    ("A1@bcde", False),
+    ("A1@bcdefghijklmno", False),
 ])
-def test_check_password(password, expected):
+def test_check_password(password: str, expected: bool) -> None:
     assert check_password(password) == expected
